6f48b07da99140033c76250af8dda03aa2f2f54a
Taissa <taissabastosgouvea@gmail.com>
2023-08-24 14:12:49 -0300

    insert funcionarios :)

---
+++ insert funcionarios.sql
@@ -0,0 +1,11 @@
+
+
+INSERT INTO funcionario (nome, cpf) VALUES 
+("João","829.617.175-90"),
+("Gabriela", "248.284.013-93"),
+("Pedro", "701.476.232-00"),
+("Maria", "661.418.542-06"),
+("Larissa", "728.068.519-63");
+
+
+
